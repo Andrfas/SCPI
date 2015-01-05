e93ff07365350cb16f5dfdedbb3ce3f9c5bbae37
andrfas <andrfas@gmail.com>
2015-01-05 21:02:40 +0200

    Добавлены методы для работы с БД

--- src/tmp/DBDataHandling.java
+++ src/tmp/DBDataHandling.java
@@ -1,20 +1,49 @@
 package tmp;
 
-import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.*;
+import tmp.tables.ArtifactType;
 import tmp.tables.Employee;
+import tmp.tables.Group;
+
+import java.util.List;
 
 /**
  * Created by Andrey on 05.01.2015.
  */
 public class DBDataHandling {
     public static void main(String[] args) {
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/mydb", "andrfas", "59722795");
-        new DBDataHandling().addEmployee("Vasya");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/InformationDB", "andrfas", "59722795");
+
     }
 
-    public void addEmployee(String name) {
+    public void addEmployee(String first_name, String last_name) {
         Employee employee = new Employee();
-        employee.set("name", name);
+        employee.set("first_name", first_name, "last_name", last_name);
         employee.saveIt();
     }
+
+    public void addGroup(String name) {
+        Group group = new Group();
+        group.set("name", name);
+        group.saveIt();
+    }
+
+    public Integer getGroupName(int id) {
+        List<Group> groupList = Group.where("id ='"+id+"'");
+        Group group = groupList.get(0);
+        return (Integer)group.get("name");
+    }
+
+    public Integer getGroupId(String name) {
+        List<Group> groupList = Group.where("name ='"+name+"'");
+        Group group = groupList.get(0);
+        return (Integer)group.get("id");
+    }
+
+    public void addArtifactType(String name, int groupId) {
+        ArtifactType artifType = new ArtifactType();
+        artifType.set("name", name, "group_id", groupId);
+        artifType.saveIt();
+    }
+
 }
