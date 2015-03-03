3a945645249844936d07aa55a2801681e7d7ba3c
Andrey Mikhatsky <andrfas@gmail.com>
2015-03-03 14:39:45 +0200

    [identification] rewrited authentification part

--- src/identificationModule/LogInServlet.java
+++ src/identificationModule/LogInServlet.java
@@ -1,5 +1,8 @@
 package identificationModule;
 
+import employeesModule.EmployeeDAO;
+import entities.employees.Employee;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -13,22 +16,34 @@ public class LogInServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        HttpSession session = req.getSession();
-        session.setAttribute("user", "Pankaj");
-        session.setMaxInactiveInterval(30*60);
+        String login = (String) req.getAttribute("login");
+        String password = (String) req.getAttribute("password");
 
-        Cookie userName = new Cookie("user", "Igor");
-        userName.setMaxAge(30*60);
-        resp.addCookie(userName);
+        EmployeeDAO emplDAO = new EmployeeDAO();
+        Employee employee = emplDAO.getEmployee(login, password);
+        if (employee != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("login", login);
+            session.setAttribute("employee", employee);
+            session.setMaxInactiveInterval(30*60);
+            resp.sendRedirect("/SCPI/actions");
+        } else {
+            resp.sendRedirect("/SCPI/login");
+        }
 
-        req.setAttribute("sessionId", session.getLastAccessedTime());
-        resp.sendRedirect("/SCPI/pages/authentication/authorisedUser.jsp");
+
+
+//        Cookie userName = new Cookie("user", "Igor");
+//        userName.setMaxAge(30*60);
+//        resp.addCookie(userName);
+
+//        req.setAttribute("sessionId", session.getLastAccessedTime());
 //        req.getRequestDispatcher("/index.html").forward(req, resp);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//        req.getRequestDispatcher("/pages/authentication/login.html").forward(req, resp);
+        req.getRequestDispatcher("/pages/authentication/login.html").forward(req, resp);
     }
 }
