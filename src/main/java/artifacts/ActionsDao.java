eea29ad4a8b25d4524f61ca729e710660d7ecf9e
Andrey Mikhatsky <andrfas@gmail.com>
2015-03-19 17:33:54 +0200

    [actions] added method for getting employee actions

---
+++ src/main/java/artifacts/ActionsDao.java
@@ -0,0 +1,41 @@
+package artifacts;
+
+import entities.artifacts.Action;
+import entities.employees.Employee;
+import hibernateConfig.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
+/**
+ * Created by Andrey on 17.03.2015.
+ */
+public class ActionsDao {
+    private static SessionFactory factory = HibernateUtil.getSessionFactory();
+
+    /* !!!
+    criteria.setFetchMode("communicationAddresses", FetchMoode.EAGER);
+     */
+
+
+    public List<Action> getActionsByEmployee(Employee empl) {
+        if (empl==null) throw new NullPointerException();
+
+        Session session = factory.getCurrentSession();
+        Transaction ta = null;
+
+        session.getTransaction().begin();
+
+        Criteria cr = session.createCriteria(Action.class);
+
+        Criterion criterion = Restrictions.like("employee", empl );
+        List<Action> list = cr.add(criterion).list();
+        return list;
+    }
+
+}
